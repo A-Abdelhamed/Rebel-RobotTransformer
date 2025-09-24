98584f30fe71e3888f6e0d3ccdb1611c33f94905
A-Abdelhamed <ahmed.abdelhamed@stud.th-owl.de>
2025-09-24 14:14:17 +0200

    Update containerTest.py

--- containerTest.py
+++ containerTest.py
@@ -1,5 +1,42 @@
-print("hello world")
-while True:{
-    
-    
-}
+# @title Imports
+
+from typing import Any, Callable, Dict, Optional, Sequence, Union, NamedTuple, Tuple
+
+import copy
+import enum
+import flax
+import flax.linen as nn
+import jax
+from jax.experimental import mesh_utils
+import jax.numpy as jnp
+import numpy as np
+import optax
+import tensorflow_datasets as tfds
+import rlds
+import reverb
+from rlds import transformations
+import tensorflow_datasets as tfds
+import tree
+
+import abc
+import dataclasses
+import math
+from typing import Dict, Optional
+
+from rlds import rlds_types
+import tensorflow as tf
+from PIL import Image
+from IPython import display
+import tensorflow_datasets as tfds
+import functools
+from typing import Callable, Sequence
+import matplotlib.pyplot as plt
+import tensorflow_hub as hub
+import orbax.checkpoint as ocp
+import os
+
+
+# Load language model and
+
+print("Num GPUs Available:", len(tf.config.list_physical_devices('GPU')))
+print("Num GPUs Available:",jax.devices())
